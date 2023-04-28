71bde733f8fb9942d71dc89afab42a01022ab0e2
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-04-28 12:16:56 -0600

    working on css

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // import { annotate, annotationGroup } from 'https://unpkg.com/rough-notation?module';
 
-// const a1 = annotate(document.getElementById('developer'), { type: 'highlight', color: 'rgb(244, 214, 244)' });
-// const a2 = annotate(document.getElementById('react'), { type: 'highlight', color: 'rgb(244, 214, 244)' });
-// const a3 = annotate(document.getElementById('javascript'), { type: 'highlight', color: 'lightyellow' });
-// const a4 = annotate(document.getElementById('css'), { type: 'highlight', color: 'lightblue' });
+// const a1 = annotate(document.getElementById('developer'), { type: 'highlight', color: 'rgb(244, 214, 244)', iterations: 1 });
+// const a2 = annotate(document.getElementById('react'), { type: 'highlight', color: 'rgb(244, 214, 244)', iterations: 1 });
+// const a3 = annotate(document.getElementById('javascript'), { type: 'highlight', color: 'lightyellow', iterations: 1 });
+// const a4 = annotate(document.getElementById('css'), { type: 'highlight', color: 'lightblue', iterations: 1 });
 
 
 // const ag = annotationGroup([a1, a2, a3, a4]);
